5a8f09912323583d391003d7e29e494f697218e8
EdwardCasler <edwardcasler@hotmail.com>
2024-11-03 10:32:22 -0800

    pizza

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OpMode/AutoParking.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OpMode/AutoParking.java
@@ -1,16 +1,18 @@
-package org.firstinspires.ftc.robotcontroller.external.samples;
+package org.firstinspires.ftc.teamcode;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.CRServo;
 import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorEx;
 
-
-
-@Autonomous(name = "AutoParking", group = "Autonomous")
+@Autonomous(name = "IntakeServoTest", group = "Autonomous")
 public class AutoParking extends LinearOpMode {
 
     double power = 0.5;
 
     private DcMotor leftFrontDrive, rightFrontDrive, leftBackDrive, rightBackDrive;
+    private CRServo intake;
+    private DcMotorEx armMotor;
 
     @Override
     public void runOpMode() {
@@ -19,6 +21,8 @@ public class AutoParking extends LinearOpMode {
         rightFrontDrive = hardwareMap.get(DcMotor.class, "left_back_drive");
         leftBackDrive = hardwareMap.get(DcMotor.class, "right_front_drive");
         rightBackDrive = hardwareMap.get(DcMotor.class, "right_back_drive");
+        intake = hardwareMap.get(CRServo.class, "intake");
+        armMotor = hardwareMap.get(DcMotorEx.class, "left_arm");
 
         leftFrontDrive.setDirection(DcMotor.Direction.REVERSE);
         leftBackDrive.setDirection(DcMotor.Direction.FORWARD);
@@ -34,9 +38,10 @@ public class AutoParking extends LinearOpMode {
         waitForStart();
 
         // Unpark: Move forward to unpark
-        move(movement.FORWARD, 0.5, 100); // Adjust power and time as needed
-        move(movement.RIGHT, 0.5, 2300);
-        move(movement.BACKWARDS, 0.5, 50);
+        IntakeRotation(intakeOperations.INTAKE_IN, 0.5f, 1);
+        IntakeRotation(intakeOperations.INTAKE_OUT, 0.5f, 1);
+
+        ArmRotation(armOperations.ARM_BACKWARDS, 1, 1);
 
 
         sleep(1000); // Pause before parking
@@ -53,26 +58,36 @@ public class AutoParking extends LinearOpMode {
         RIGHT
     }
 
+    enum intakeOperations {
+        INTAKE_IN,
+        INTAKE_OUT
+    }
+
+    enum armOperations {
+        ARM_FORWARD,
+        ARM_BACKWARDS
+    }
+
     private void move(movement movement, double power, int duration) {
-        if(movement.equals(AutoParking.movement.FORWARD))  {
+        if(movement == AutoParking.movement.FORWARD)  {
             leftFrontDrive.setPower(power);
             rightFrontDrive.setPower(power);
             leftBackDrive.setPower(power);
             rightBackDrive.setPower(power);
 
-        } else if(movement.equals(AutoParking.movement.BACKWARDS)) {
+        } else if(movement == AutoParking.movement.BACKWARDS) {
             leftFrontDrive.setPower(-power);
             rightFrontDrive.setPower(-power);
             leftBackDrive.setPower(-power);
             rightBackDrive.setPower(-power);
 
-        } else if(movement.equals(AutoParking.movement.LEFT)) {
+        } else if(movement == AutoParking.movement.LEFT) {
             leftFrontDrive.setPower(-power);
             rightFrontDrive.setPower(power);
             leftBackDrive.setPower(power);
             rightBackDrive.setPower(-power);
 
-        } else if(movement.equals(AutoParking.movement.RIGHT)) {
+        } else if(movement == AutoParking.movement.RIGHT) {
             leftFrontDrive.setPower(power);
             rightFrontDrive.setPower(-power);
             leftBackDrive.setPower(-power);
@@ -82,21 +97,23 @@ public class AutoParking extends LinearOpMode {
         stopMotors();
     }
 
-    // Helper methods for movement
-    private void moveForward(double power, int duration) {
-        leftFrontDrive.setPower(power);
-        rightFrontDrive.setPower(power);
-        leftBackDrive.setPower(power);
-        rightBackDrive.setPower(power);
+    private void IntakeRotation(intakeOperations operation, double power, int duration) {
+        if(operation == intakeOperations.INTAKE_IN) {
+            intake.setPower(power);
+        } else if(operation == intakeOperations.INTAKE_OUT) {
+            intake.setPower(-power);
+        }
         sleep(duration);
+        stopIntake();
     }
-
-    private void moveBackward(double power, int duration) {
-        leftFrontDrive.setPower(-power);
-        rightFrontDrive.setPower(-power);
-        leftBackDrive.setPower(-power);
-        rightBackDrive.setPower(-power);
+    private void ArmRotation(armOperations operation, double power, int duration) {
+        if(operation == armOperations.ARM_FORWARD) {
+            armMotor.setPower(power);
+        } else if(operation == armOperations.ARM_BACKWARDS) {
+            armMotor.setPower(-power);
+        }
         sleep(duration);
+        stopArm();
     }
 
     private void stopMotors() {
@@ -105,12 +122,10 @@ public class AutoParking extends LinearOpMode {
         leftBackDrive.setPower(0);
         rightBackDrive.setPower(0);
     }
-
-    private void moveRight(double power, int duration) {
-        leftFrontDrive.setPower(power);
-        rightFrontDrive.setPower(-power);
-        leftBackDrive.setPower(-power);
-        rightBackDrive.setPower(power);
-        sleep(duration);
+    private void stopIntake() {
+        intake.setPower(0);
+    }
+    private void stopArm() {
+        armMotor.setPower(0);
     }
 }
